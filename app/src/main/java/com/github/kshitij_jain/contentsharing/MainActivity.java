0c7ef300ae9fc6deb1ee9ac6287f3773f7d63fbb
Kshitij Jain <kshitij.jain007.kj@gmail.com>
2017-02-02 16:50:32 +0530

    Main Activity: Easy Share Action Added

--- app/src/main/java/com/github/kshitij_jain/contentsharing/MainActivity.java
+++ app/src/main/java/com/github/kshitij_jain/contentsharing/MainActivity.java
@@ -2,14 +2,20 @@ package com.github.kshitij_jain.contentsharing;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +71,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Easy Share Action
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if (mShareActionProvider != null) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, "Share Text");
+            intent.setType("text/plain");
+            mShareActionProvider.setShareIntent(intent);
+        }
+        return true;
     }
 
 }
